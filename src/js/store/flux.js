79f1f734ff1a9d21d2cfdd3e1fbdd9f1817875f7
MaRau12 <marthaelisabeth94@gmail.com>
2023-05-29 12:38:13 +0000

    how to store from form in store?

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -6,18 +6,26 @@ const getState = ({ getStore, getActions, setStore }) => {
 				{id:2 ,name: "James", phone:345}, 
 				{id:3 ,name: "Hanna", phone:456}, 
 				{id:4 ,name: "Lisa", phone:567}
-			]
+			],
+			addThisContact: {}
 				
 			
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
 
-			addAContact: () => {
-
+			addAContact: async() => {
+			const response = await fetch(assets.breatheco.de/apis/fake/contact/{
+			methods: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(store.newContact)
+	     	});
 			},
 			
-			loadSomeData: () => {
+			getAllContacts: async () => {
+				const response = await fetch(assets.breatheco.de/apis/fake/contact/agenda)
+				const data = await response.json();
+				setStore({contacts: data.contacts})
 				/**
 					fetch().then().then(data => setStore({ "foo": data.bar }))
 				*/
--- src/js/views/form.js
+++ src/js/views/form.js
@@ -1,23 +1,24 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, setStore } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 
 export const Form = () => {
     const { store, actions } = useContext(Context);
-    const {contacts, setContacts } = useState([]);
+   /* const {contacts, setContacts } = useState([]); */
+    const [newContact, setNewContact] = useState ({full_name: "",  email: "", address: "", phone: ""})
+    console.log(newContact)
+    const handleContact = () => {
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-       let newId = 0;
-       for (let x = 0; x < contacts.length; x++){
-        if(contacts[x].id > newId){
-            newId = contacts[x].id;
+      /* let newId = 0;
+       for (let x = 0; x < store.contacts.length; x++){
+        if(store.contacts[x].id > newId){
+            newId = newContact[x].id;
         }
-       };
-       
-       let newContact = {id: newId + 1, name: event.target.value, email: event.target.value, phone: event.target.value, address: event.target.value}
-       setContacts([...contacts, newContact])
+       }; */
+       console.log(newContact)
+       setStore({addThisContact: newContact})
+       console.log(store.addThisContact)
         
       }; 
     
@@ -27,30 +28,30 @@ export const Form = () => {
     <div className="container"> 
     <h1>Add a new Contact</h1>
 
-    <form onSubmit={handleSubmit}>
+    <form>
 
   <div className="mb-3">
-    <label for="name" className="form-label">Full Name</label>
-    <input type="Text" className="form-control" id="name" value={name}></input>
+    <label for="full_name" className="form-label">Full Name</label>
+    <input type="Text" className="form-control" id="full_name" value={newContact.full_name} onChange={(e)=> setNewContact({...newContact, full_name: e.target.value})}></input>
     
-  </div>
+  </div> 
   <div className="mb-3">
     <label for="email" className="form-label">Email address</label>
-    <input type="text" className="form-control" id="email" value={email} ></input>
+    <input type="text" className="form-control" id="email" value={newContact.email} onChange={(e)=> setNewContact({...newContact, email: e.target.value})}></input>
    
   </div>
   <div className="mb-3">
     <label for="phone" className="form-label">Phone Number</label>
-    <input type="number" className="form-control" id="phone" value={phone}></input>
+    <input type="number" className="form-control" id="phone" value={newContact.phone} onChange={(e)=> setNewContact({...newContact, phone: e.target.value})}></input>
     
   </div>
   <div className="mb-3">
     <label for="address" className="form-label">Address</label>
-    <input type="Text" className="form-control" id="address" value={address}></input>
+    <input type="Text" className="form-control" id="address" value={newContact.address} onChange={(e)=> setNewContact({...newContact, address: e.target.value})}></input>
     
   </div>
  
-  <button type="submit" className="btn btn-primary" >Submit</button>
+  <button onClick={handleContact()} className="btn btn-primary" >Submit</button>
   <Link to="/">go back to contacts</Link>
 
 </form>
