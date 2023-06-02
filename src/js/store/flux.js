24362f17b6c4cb438e87e8ad3e42b3fe146599fe
MaRau12 <marthaelisabeth94@gmail.com>
2023-06-02 15:46:37 +0000

    changed newContact to array

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -5,25 +5,31 @@ const getState = ({ getStore, getActions, setStore }) => {
 				{id:1 ,name: "Fred", phone: 123}, 
 				{id:2 ,name: "James", phone:345}, 
 				{id:3 ,name: "Hanna", phone:456}, 
-				{id:4 ,name: "Lisa", phone:567}
+				{id:4 ,name: "Lisa", phone:567},
+                
 			],
-			addThisContact: {}
+			newContact: [],
 				
 			
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
+            handleContact: (contactToAdd)=>{
+				const store = getStore();
+				setStore({newContact: [...store.newContact, contactToAdd]})
+				console.log(store.newContact)
+			},
 
 			addAContact: async() => {
 			const response = await fetch(assets.breatheco.de/apis/fake/contact/{
 			methods: "POST",
 			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify(store.newContact)
+			body: JSON.stringify(newContact)
 	     	});
 			},
 			
 			getAllContacts: async () => {
-				const response = await fetch(assets.breatheco.de/apis/fake/contact/agenda)
+				const response = await fetch("assets.breatheco.de/apis/fake/contact/agenda/agenda_M")
 				const data = await response.json();
 				setStore({contacts: data.contacts})
 				/**
--- src/js/views/form.js
+++ src/js/views/form.js
@@ -7,21 +7,26 @@ export const Form = () => {
     const { store, actions } = useContext(Context);
    /* const {contacts, setContacts } = useState([]); */
     const [newContact, setNewContact] = useState ({full_name: "",  email: "", address: "", phone: ""})
-    console.log(newContact)
-    const handleContact = () => {
+    
 
-      /* let newId = 0;
+    const submitContact = () => {
+      console.log("Helloooooo")
+       /*let newId = 0;
        for (let x = 0; x < store.contacts.length; x++){
         if(store.contacts[x].id > newId){
             newId = newContact[x].id;
         }
        }; */
-       console.log(newContact)
-       setStore({addThisContact: newContact})
-       console.log(store.addThisContact)
+       actions.handleContact(newContact)
+      /* console.log(newContact)*/
+      
+      /* actions.addAContact();*/
         
       }; 
-    
+    const test = () => {
+      console.log(store.newContact)
+    }
+
    
 
   return (
@@ -31,27 +36,28 @@ export const Form = () => {
     <form>
 
   <div className="mb-3">
-    <label for="full_name" className="form-label">Full Name</label>
+    <label htmlFor="full_name" className="form-label">Full Name</label>
     <input type="Text" className="form-control" id="full_name" value={newContact.full_name} onChange={(e)=> setNewContact({...newContact, full_name: e.target.value})}></input>
     
   </div> 
   <div className="mb-3">
-    <label for="email" className="form-label">Email address</label>
+    <label htmlFor="email" className="form-label">Email address</label>
     <input type="text" className="form-control" id="email" value={newContact.email} onChange={(e)=> setNewContact({...newContact, email: e.target.value})}></input>
    
   </div>
   <div className="mb-3">
-    <label for="phone" className="form-label">Phone Number</label>
+    <label htmlFor="phone" className="form-label">Phone Number</label>
     <input type="number" className="form-control" id="phone" value={newContact.phone} onChange={(e)=> setNewContact({...newContact, phone: e.target.value})}></input>
     
   </div>
   <div className="mb-3">
-    <label for="address" className="form-label">Address</label>
+    <label htmlFor="address" className="form-label">Address</label>
     <input type="Text" className="form-control" id="address" value={newContact.address} onChange={(e)=> setNewContact({...newContact, address: e.target.value})}></input>
     
   </div>
  
-  <button onClick={handleContact()} className="btn btn-primary" >Submit</button>
+  <button onClick={submitContact} className="btn btn-primary" >Submit</button>
+  <button onClick={test} className="btn btn-primary" >Test</button>
   <Link to="/">go back to contacts</Link>
 
 </form>
